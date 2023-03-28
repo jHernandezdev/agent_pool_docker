0b81f74bcd5e779b217de48aa4011f4f87f938c5
Jose María Hernández Hernández <jose.herher@gmail.com>
2023-03-28 07:41:38 +0200

    Added secret

--- dockeragent/run.sh
+++ dockeragent/run.sh
@@ -1,1 +1,2 @@
 docker run -e AZP_URL="https://dev.azure.com/jHernandez-Dev" -e AZP_TOKEN="eqcjhaz2qn2xcgpkyl2i3jgvyjltvnxlafyizexhf4hnduzzegda" dockeragent:latest
+kubectl create secret docker-registry jmhhacr-key --docker-server="jmhhcontainerregistry.azurecr.io" --docker-username="jmhhcontainerregistry" --docker-password="YUpQ44BDrwr5A7XwwChNHgNBlw/kFvroro9VRi88Pk+ACRBS8kPl"
